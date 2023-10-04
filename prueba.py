2ac9c335a92ed30ee034abf3b7d35f8ba52a3cc3
Juan Esteban Brand Tovar <brand.juan@correounivalle.edu.co>
2023-10-03 22:21:54 -0500

    fuerza bruta parcial

--- prueba.py
+++ prueba.py
@@ -21,9 +21,6 @@ for perm in permutaciones_estudiantes:
         asignaciones.append(asignacion)
     combinaciones_finales.append(asignaciones)
 
-# Ahora, combinaciones_finales contiene todas las combinaciones en el formato original
-# Puedes acceder a ellas o realizar otras operaciones según tus necesidades.
-
 # Diccionario de materias y sus cupos disponibles
 materias_cupos = {
     "MateriaA": 2,
@@ -46,6 +43,9 @@ def generar_asignaciones(estudiantes, materias_cupos):
     
     return asignaciones
 
+# Variable para almacenar las asignaciones finales
+asignaciones_finales = []
+
 # Aplicar la función a cada lista interna
 for lista_estudiantes in combinaciones_finales:
     # Reiniciar los cupos para cada lista interna
@@ -53,7 +53,64 @@ for lista_estudiantes in combinaciones_finales:
     
     asignaciones = generar_asignaciones(lista_estudiantes, materias_cupos_temp)
     
-    # Imprimir las asignaciones para cada lista interna
-    for estudiante, materias in asignaciones.items():
-        print(f"{estudiante}: {', '.join(materias)}")
-    print()
+    # Agregar las asignaciones a la lista de asignaciones_finales
+    asignaciones_finales.append(asignaciones)
+
+# Ahora, asignaciones_finales contiene las asignaciones de todas las combinaciones de estudiantes
+# Puedes acceder a ellas según tus necesidades.
+def sublista(lista1, lista2):
+    return [x for x in lista1 if x not in lista2]
+
+def convierte(diccionario):
+    lista = []
+    for materia, prioridad in diccionario.items():
+            lista.append(materia)
+    return lista
+
+def obtener_prioridad(nombre_estudiante, nombre_materia):
+    for estudiante in estudiantes:
+        if estudiante["nombre"] == nombre_estudiante:
+            prioridades = estudiante["prioridades"]
+            if nombre_materia in prioridades:
+                return prioridades[nombre_materia]
+            
+def cuenta_prioridad(diccionario):
+    cuenta = 0
+    for materia, prioridad in diccionario.items():
+            if(prioridad!=0):
+                cuenta+=1
+    return cuenta
+            
+def calcular_insatisfaccion(asignaciones, estudiantes):
+    insatisfaccion_general = 0.0  # Inicializar con un valor decimal
+    
+    for estudiante in estudiantes:
+        estudiante_nombre = estudiante["nombre"]
+        prioridades = estudiante["prioridades"]
+        asignacion_estudiante = asignaciones[estudiante_nombre]
+        diferencia = sublista(convierte(prioridades),asignacion_estudiante)
+        diferencia_ab = 0
+        
+        for elemento in diferencia:
+            diferencia_ab+= obtener_prioridad(estudiante_nombre, elemento)
+
+        insatisfaccion_general += abs((1-(len(asignacion_estudiante)/cuenta_prioridad(prioridades)))* diferencia_ab)
+
+
+    return round(insatisfaccion_general, 2)  # Redondear a 2 decimales
+
+# Calcular la insatisfacción para cada asignación en asignaciones_finales
+insatisfacciones = []
+
+for asignacion in asignaciones_finales:
+    insatisfaccion = calcular_insatisfaccion(asignacion, estudiantes)
+    insatisfacciones.append(insatisfaccion)
+
+# La lista 'insatisfacciones' contiene la insatisfacción de cada asignación en el mismo orden que 'asignaciones_finales'.
+
+
+minimo = min(insatisfacciones)
+index = insatisfacciones.index(minimo)
+final = asignaciones_finales[0]
+
+print(final)
